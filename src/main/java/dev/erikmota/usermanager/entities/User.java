73a2aea05a793226f2bb09778c280e61bea8cdd8
Erik Mota <seriikmota.dev@gmail.com>
2024-12-17 13:57:09 -0300

    feat: add entity and dto`s for user

---
+++ src/main/java/dev/erikmota/usermanager/entities/User.java
@@ -0,0 +1,36 @@
+package dev.erikmota.usermanager.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private Boolean enabled;
+
+}
