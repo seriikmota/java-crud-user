464fda36854f3715602b863605cde9d8c131cbaf
Erik Mota <seriikmota.dev@gmail.com>
2025-02-11 11:26:24 -0300

    config: alter default url swagger

--- src/main/java/dev/erikmota/usermanager/UserManagerApplication.java
+++ src/main/java/dev/erikmota/usermanager/UserManagerApplication.java
@@ -1,10 +1,17 @@
 package dev.erikmota.usermanager;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "/", description = "Default Server URL")
+		}
+)
 public class UserManagerApplication {
 
 	public static void main(String[] args) {
